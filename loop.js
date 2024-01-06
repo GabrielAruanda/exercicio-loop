600b0f1f2c8cb52a97c6c6b593cc5191fcaba006
GabrielAruanda <garuanda@gmail.com>
2024-01-05 17:43:39 -0800

    first commit

---
+++ loop.js
@@ -0,0 +1,40 @@
+
+//    1
+//    let nome
+//    nome = prompt ("Qual é o seu nome")
+//    alert (nome)
+//    let senha
+//    senha = prompt ("Insira a sua senha:")
+   
+//    while(nome == senha){ 
+//     alert ("Senha invalida")
+
+
+//     senha = prompt ("Insira a sua senha:")
+   
+//    senha = prompt ("Insira novamente a senha!")
+
+//    2
+//    let nome
+//    nome = prompt("Qual é o seu nome?")
+//    alert (nome)
+//    while(nome.length<=3){
+//    alert("Nome menor que 3 caracteres")
+//    nome = prompt("Insira o nome novamente:")
+//    }
+//    let idade
+//    idade = prompt("Qual a sua idade?")
+//    alert ("Sua idade é:") 
+//    alert (idade)
+//    let salario
+//    salario = prompt("Quanto você recebe de salario?")
+//    alert ("Seu salario é:")
+//    alert(salario)
+//    let genero
+//    genero = prompt("Qual o seu genero (M),(F),(ND)?")
+//    alert("Seu genero é:") 
+//    alert (genero)
+//    let estado
+//    estado = prompt("Qual seu estado sivil (S) (C) (V) (D)")
+//    alert("Estado sivil:")
+//    alert (estado)
